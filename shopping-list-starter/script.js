d03a7f00649d6f4f990779114b78e802b1c2dd35
lisa.todesco <todesco@gmail.com>
2024-11-11 16:19:47 +0800

    shopping cart updates

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,26 @@
-const addItem = () => {
+let shoppingListItems = ["milk", "eggs", "bread"];
+updateItems();
 
+function addItem () {
+    let itemInput = document.getElementById("new-item-text");
+    let item = itemInput.value.trim();
+    shoppingListItems = [...shoppingListItems, item];
+    updateItems();
+    itemInput.value = "";
 };
 
-const updateItems = () => {
+function updateItems () {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
 
+    for (const shoppingItem of shoppingListItems) {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = shoppingItem;
+        listElement.appendChild(itemElement);
+    }
+};
+
+function clearList () {
+    shoppingListItems = [];
+    updateItems();
 };
